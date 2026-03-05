f43de58a3e95a4d74ccdafcf378a98a147b8091c
Jose de Lima <delimajm@gmail.com>
2026-03-05 16:24:02 -0600

    created player class and redered player on screen

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
+
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     game_clock = pygame.time.Clock()
     dt = 0
+    
+    player_position = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    player = Player(player_position[0], player_position[1])
 
     while True:
         log_state()
+        player.draw(screen=screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -1,7 +1,20 @@
-from constants import PLAYER_RADIUS
+import pygame
+
+from constants import PLAYER_RADIUS, LINE_WIDTH
 from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x: int, y: int):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius # type: ignore
+        b = self.position - forward * self.radius - right # type: ignore
+        c = self.position - forward * self.radius + right # type: ignore
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
