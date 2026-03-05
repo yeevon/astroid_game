4a812f9d78ea409de20d568986df5ac8730bf603
Jose de Lima <delimajm@gmail.com>
2026-03-05 12:46:07 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print("Starting Asteroids with pygame version: " + pygame.version.ver)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
