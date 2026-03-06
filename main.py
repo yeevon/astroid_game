acfb7b72556b124d96e7cadce1bf11ed41f22a1c
Jose de Lima <delimajm@gmail.com>
2026-03-05 23:44:34 -0600

    created auto generating astroids

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import pygame
 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+    player_position = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+    Player.containers = (updatable, drawable) # type: ignore
+    Player(player_position[0], player_position[1])
+
+    Asteroid.containers = (asteroids, updatable, drawable) # type: ignore
+    AsteroidField.containers = (updatable) # type: ignore
+    AsteroidField()
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     game_clock = pygame.time.Clock()
     dt = 0
     
-    player_position = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
-    player = Player(player_position[0], player_position[1])
-    
-
     while True:
         log_state()
-        updatable.update(dt)
-        
+        updatable.update(dt)        
         for draw in drawable: draw.draw(screen)
-
-        # player.update(dt=dt)
-        # player.draw(screen=screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
